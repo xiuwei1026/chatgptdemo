aea7ce30d02e8daa48b73ed87f1c081503478531
Xiupeng Wei <xwei@dssinc.com>
2023-02-06 14:38:28 -0500

    first commit

---
+++ chatgpt_app.py
@@ -0,0 +1,55 @@
+# Importing required packages
+import streamlit as st
+import openai
+
+
+st.title("Chatting with AI Bot ChatGPT")
+st.sidebar.header("Instructions")
+st.sidebar.info(
+    '''You are able to talk to AI Bot ChatGPT.
+       Enter a **question** in the **text box** and **press enter** to receive 
+       a **response** from the ChatGPT
+       '''
+    )
+    
+    
+# Set the model engine and your OpenAI API key
+model_engine = "text-davinci-003"
+openai.api_key = "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" #follow step 4 to get a secret_key
+
+
+
+def main():
+    '''
+    This function gets the user input, pass it to ChatGPT function and 
+    displays the response
+    '''
+    # Get user input
+    user_query = st.text_input("Enter question here, to exit enter :q", "Talk to AI bot?")
+    if user_query != ":q" or user_query != "":
+        # Pass the query to the ChatGPT function
+        response = ChatGPT(user_query)
+        return st.write(f"{user_query} {response}")
+        
+        
+
+def ChatGPT(user_query):
+    ''' 
+    This function uses the OpenAI API to generate a response to the given 
+    user_query using the ChatGPT model
+    '''
+    # Use the OpenAI API to generate a response
+    completion = openai.Completion.create(
+                                  engine = model_engine,
+                                  prompt = user_query,
+                                  max_tokens = 1024,
+                                  n = 1,
+                                  temperature = 0.5,
+                                      )
+    response = completion.choices[0].text
+    return response
+    
+    
+    
+# call the main function
+main()     
